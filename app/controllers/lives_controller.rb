3f1875019bbbc8a584b85b52b566c29b307d898d
Honor Lin <tenhon.lin@gmail.com>
2015-06-10 20:02:24 +0800

    can edit life

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -1,6 +1,8 @@
 class LivesController < ApplicationController
 
   before_action :login_required
+  before_action :find_select_items, only: [ :new, :edit]
+
 
   def index
     @lives = current_user.lives.all
@@ -10,13 +12,22 @@ class LivesController < ApplicationController
   end
 
   def edit
+    @life = Life.find(params[:id])
+  end
+
+  def update
+    @life = Life.find(params[:id])
+
+    if @life.update_attributes(life_params)
+      redirect_to lives_path
+    else
+      render :new
+    end
+
   end
 
   def new
     @life = Life.new
-    @tags = current_user.tags
-    @locations = current_user.locations
-    @companions = current_user.companions
   end
 
   def create
@@ -32,6 +43,12 @@ class LivesController < ApplicationController
   end
 
   private
+
+  def find_select_items
+    @tags = current_user.tags
+    @locations = current_user.locations
+    @companions = current_user.companions
+  end
 
   def life_params
     params.require("life").permit("start_time", "end_time", "description", "location", "tags_all", "companion")
